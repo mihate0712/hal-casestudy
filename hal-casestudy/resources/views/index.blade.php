e82d893fe2bd68b46171516433a3358eee65a51e
LAPTOP-AGLNDO0E\村上健太 <murakami.kenta01@gmail.com>
2021-12-22 00:09:55 +0900

    TOP画面のHTMLほぼ完成、CSSと画像のみ調整予定

--- hal-casestudy/resources/views/index.blade.php
+++ hal-casestudy/resources/views/index.blade.php
@@ -9,48 +9,105 @@
 </head>
 <body>
     <header>ヘッダー部分</header>
+    <nav>
+        <form action="" method="get">
+            <input type="search" name="search" id="search" placeholder="キーワードを入力">
+            <input type="submit" name="submit" id="submit" value="検索">
+        </form>
+        <p id="syousai">詳細検索は<input type="button" value="こちら"></p>
+    </nav>
     <h1><img src="{{ asset('images/main.jpg')}}"></h1>
     <h2>あなたにオススメ</h2>
     <div class="flex">
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>おすすめだよ</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>おすすめだよ</p>
+            </a>
         </div>
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>おすすめだ</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>おすすめだ</p>
+            </a>
         </div>
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>おすすめだね</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>おすすめだね</p>
+            </a>
         </div>
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>おすすめ</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>おすすめ</p>
+            </a>
         </div>
     </div>
     <h2>人気車両ランキング</h2>
     <div class="flex">
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>人気No1</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>人気No1</p>
+            </a>
         </div>
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>人気No2</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>人気No2</p>
+            </a>
         </div>
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>人気No3</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>人気No3</p>
+            </a>
         </div>
         <div class="content">
-            <img src="{{ asset('images/sub.jpg')}}">
-            <p>人気No4</p>
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>人気No4</p>
+            </a>
         </div>
     </div>
     <h2>今週開催のオークション</h2>
     <div class="content2">
-        <img id="ork" src="{{ asset('images/ork.jpg')}}">
+        <a href="/">
+            <img id="ork" src="{{ asset('images/sub.jpg')}}">
+        </a>
     </div>
+    <h2>今後開催予定のオークション</h2>
+    <div class="flex">
+        <div class="content">
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>1月15日開催予定</p>
+                <p>車種名</p>
+            </a>
+        </div>
+        <div class="content">
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>1月15日開催予定</p>
+                <p>車種名</p>
+            </a>
+        </div>
+        <div class="content">
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>1月15日開催予定</p>
+                <p>車種名</p>
+            </a>
+        </div>
+        <div class="content">
+            <a href="/">
+                <img src="{{ asset('images/sub.jpg')}}">
+                <p>1月15日開催予定</p>
+                <p>車種名</p>
+            </a>
+        </div>
+    </div>
+    <footer>フッター部分</footer>
 </body>
 </html>
