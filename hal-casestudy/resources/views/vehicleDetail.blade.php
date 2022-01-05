54e4442e53fc1e4eb4bf4bc2834ab7eaddba204f
mihate0712 <mihate0712@icloud.com>
2022-01-06 02:54:37 +0900

    #6 vehicle_detail 車両詳細画面HTML・CSSとりあえず完成

---
+++ hal-casestudy/resources/views/vehicleDetail.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author">
+        <title>車両詳細</title>
+        <link rel="stylesheet" href="./css/vehicleDetail.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            ヘッダーです。
+        </header>
+        <div class="displayArea">
+            <h1 class="pageTitle">DAIHATSU WAKE</h1>
+            <div class="carImages">
+                <div class="carImageMain">
+                    <img src="./test.jpg" class="carImageFirst">
+                </div>
+                <div class="carImageSub">
+                    <img src="./test.jpg" class="carImageSecond">
+                    <img src="./test.jpg" class="carImageThird">
+                </div>
+            </div>
+            <div class="details">
+                <h2 class="carName">車両名：DAIHATSU WAKE</h2>
+                <table class="carDetail">
+                    <tr>
+                        <th class="th">車両形式</th>
+                        <td class="td">平成30年物</td>
+                    </tr>
+                    <tr>
+                        <th class="th">車両形式</th>
+                        <td class="td">平成30年物</td>
+                    </tr>
+                    <tr>
+                        <th class="th">車両形式</th>
+                        <td class="td">平成30年物</td>
+                    </tr>
+                    <tr>
+                        <th class="th">車両形式</th>
+                        <td class="td">平成30年物</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="buttons">
+                <input type="button" class="searchAlike" name="searchAlike" value="似た形式の車両を探す">
+                <input type="button" class="goToAuction" name="goToAuction" value="オークション画面に遷移">
+                <input type="button" class="Favorite" name="Favorite" value="この車両をお気に入り登録">
+                <input type="button" class="back" name="back" value="車両一覧画面に戻る">
+            </div>
+        </div>
+        <footer>
+            ふったーだよ
+        </footer>
+    </body>
+</html>
