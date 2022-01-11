2f295e3984c55ce0e45d7ccf177aeeb028be6149
HikaruNakano1019 <84007991+HikaruNakano1019@users.noreply.github.com>
2022-01-11 15:11:55 +0900

    login
    html,css完成

---
+++ hal-casestudy/resources/views/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+  <title>ログイン</title>
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:700,400">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
+  <link rel="stylesheet" href="css/destyle.css">
+  <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+
+  <body>
+
+    <div id="loginForm">
+      <div id="logo">
+        <div style="color: black;">ロゴ</div>
+      </div>
+
+      <fieldset>
+        <h1>ログイン</h1>
+        <form>
+          <div class="iconUser"></div>
+          <input type="text" placeholder="メールアドレス" required>
+          <div class="iconPassword"></div>
+          <input type="password" placeholder="パスワード" required>
+          <input type="submit" value="ログイン">
+        </form>
+      </fieldset>
+
+    </div>
+  </body>
+
+
+</body>
+
+</html>
