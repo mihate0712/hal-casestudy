fed039c904d6295020a652d931fbedef53000130
LAPTOP-AGLNDO0E\村上健太 <murakami.kenta01@gmail.com>
2022-01-21 16:02:48 +0900

    タイマーの非同期処理完成

--- hal-casestudy/resources/views/auction.blade.php
+++ hal-casestudy/resources/views/auction.blade.php
@@ -1,3 +1,7 @@
+<?php
+    $price = 100000;
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -12,43 +16,100 @@
     <h1>オークション画面</h1>
     <div id="nav">
         <p>車両名 トヨタプリウス（型式 DAA-ZVW51）
-        <p>オークション中（ステータス）</p>
     </div>
-    <h2 id="main">
-        <img src="{{ asset('images/auction.jpg')}}">
-        <div id="spec">
-            <p>基本情報</p>
-            <dl>
-                <dt>年式</dt>
-                <dd>2020年</dd>
-                <dt>走行</dt>
-                <dd>0.8万km</dd>
-                <dt>排気量</dt>
-                <dd>1800cc</dd>
-                <dt>車検</dt>
-                <dd>2023年4月</dd>
-                <dt>乗車定員</dt>
-                <dd>5</dd>
-                <dt>燃料</dt>
-                <dd>ガソリン</dd>
-                <dt>駆動方式</dt>
-                <dd>2WD</dd>
-                <dt>ボディタイプ</dt>
-                <dd>セダン</dd>
-                <dt>ミッション</dt>
-                <dd>AT</dd>
-                <dt>色</dt>
-                <dd>シルバー</dd>
-                <dt>ハンドル</dt>
-                <dd>右</dd>
-                <dt>車歴</dt>
-                <dd>自家用車</dd>
-                <dt>型式</dt>
-                <dd>DAA-ZVW51</dd>
-                <dt>ドア数</dt>
-                <dd>5</dd>
-            </dl>
+    <div id="main">
+        <div id="left">
+            <img src="{{ asset('images/auction.jpg')}}">
         </div>
-    </h2>
+        <div id="right">
+            <div class="timer" id="timer"></div>
+            <div id="price_text">現在の価格: <p id="price">{{$price}}</p>円</div>
+            <button class="button" type="button" onclick="onClick();">入札</button>
+        </div>
+    </div>
+    <div id="spec">
+        <table>
+            <thead>
+                <th colspan="4">基本情報</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>年式</th>
+                    <td>2020年</td>
+                    <th>駆動方式</th>
+                    <td>2WD</td>
+                </tr>
+                <tr>
+                    <th>走行</th>
+                    <td>0.8万km</td>
+                    <th>ボディタイプ</th>
+                    <td>セダン</td>
+                </tr>
+                <tr>
+                    <th>排気量</th>
+                    <td>1800cc</td>
+                    <th>ミッション</th>
+                    <td>AT</td>
+                </tr>
+                <tr>
+                    <th>車検</th>
+                    <td>2023年4月</td>
+                    <th>色</th>
+                    <td>シルバー</td>
+                </tr>
+                <tr>
+                    <th>乗車定員</th>
+                    <td>5</td>
+                    <th>ハンドル</th>
+                    <td>右</td>
+                </tr>
+                <tr>
+                    <th>燃料</th>
+                    <td>ガソリン</td>
+                    <th>型式</th>
+                    <td>DAA-ZVW51</td>
+                </tr>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <th colspan="4">オプション</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>PS（パワー捨て）</th>
+                    <td></td>
+                    <th>PW（パワーウインド）</th>
+                    <td>〇</td>
+                </tr>
+                <tr>
+                    <th>AW（サンルーフ）</th>
+                    <td></td>
+                    <th>背面タイヤ</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>カワ（本革シート）</th>
+                    <td></td>
+                    <th>AB（エアーバック）</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>TV（テレビ）</th>
+                    <td></td>
+                    <th>ナビ（カーナビ）</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Rスポ（リヤースボイラ）</th>
+                    <td></td>
+                    <th></th>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <footer>フッター部分</footer>
+    <script src="{{ asset('/js/auction.js') }}"></script>
 </body>
 </html>
